55cb238fb58568f1c47c6db51d69b7dff607142a
Jungsoo Kim <urlicht@users.noreply.github.com>
2020-09-15 20:37:00 -0400

    Update FlavellPkg.jl

--- src/FlavellPkg.jl
+++ src/FlavellPkg.jl
@@ -1,10 +1,12 @@
 module FlavellPkg
 
+include("py.jl")
 include("default.jl")
 include("imaging.jl")
 include("behavior.jl")
 
-export install_default,
+export install_py,
+    install_default,
     install_imaging,
     install_behavior
 
