739b20d80968373ca40e956df82585bfbd879ce8
urlicht <jungsu93@gmail.com>
2020-09-20 11:26:03 -0400

    fix precomile error for url added pkg

--- src/default.jl
+++ src/default.jl
@@ -3,26 +3,29 @@ import Pkg
 function install_default(use_julia_conda=true)
     # pycall, pyplot
     install_py(use_julia_conda)
-    
+
     pkg_list = []
-    
+
     # add private pkg
     for pkg = ["FlavellBase"]
-        push!(pkg_list, Pkg.PackageSpec(url="git@github.com:flavell-lab/$(pkg).jl.git"))
+        push!(pkg_list, Pkg.PackageSpec(name=pkg,
+            url="git@github.com:flavell-lab/$(pkg).jl.git"))
     end
-    
+
     # add registered pkg
-    for pkg = ["IJulia", "HDF5", "NaNMath", "LsqFit", "Optim", "Interpolations", "Distributions",
-            "DataStructures", "BenchmarkTools", "ProgressMeter", "Images", "Clustering", "MultivariateStats"]
+    for pkg = ["IJulia", "HDF5", "NaNMath", "LsqFit", "Optim", "Interpolations",
+         "Distributions", "DataStructures", "BenchmarkTools", "ProgressMeter",
+         "Images", "Clustering", "MultivariateStats"]
         push!(pkg_list, pkg)
     end
-    
+
     # adding
     for pkg = pkg_list
       Pkg.add(pkg)
     end
     # precompiling
     for pkg = pkg_list
-      eval(:(using $(Symbol(pkg))))
+        pkg_name = isa(pkg, Pkg.Types.PackageSpec) ? pkg.name : pkg
+        eval(:(using $(Symbol(pkg))))
     end
 end
--- src/imaging.jl
+++ src/imaging.jl
@@ -2,18 +2,20 @@ import Pkg
 
 function install_imaging()
     pkg_list=  []
-    
+    # add private pkg
     for pkg = ["MHDIO", "ImageDataIO", "WormCurveFinder",
         "WormFeatureDetector", "SegmentationTools", "CaAnalysis", "ND2Process"]
-        push!(pkg_list, Pkg.PackageSpec(url="git@github.com:flavell-lab/$(pkg).jl.git"))
+        push!(pkg_list, Pkg.PackageSpec(name=pkg,
+            url="git@github.com:flavell-lab/$(pkg).jl.git"))
     end
-        
+
     # adding
     for pkg = pkg_list
         Pkg.add(pkg)
     end
     # precompiling
     for pkg = pkg_list
+        pkg_name = isa(pkg, Pkg.Types.PackageSpec) ? pkg.name : pkg
         eval(:(using $(Symbol(pkg))))
     end
 end
