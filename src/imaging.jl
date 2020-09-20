50fa0c58f365c0fe2ef931adca76b60a0896b6da
Jungsoo Kim <urlicht@users.noreply.github.com>
2020-09-20 11:12:44 -0400

    Update imaging.jl
    installation pkg spec abstraction

--- src/imaging.jl
+++ src/imaging.jl
@@ -1,13 +1,17 @@
 import Pkg
 
 function install_imaging()
-    pkg_list = ["MHDIO", "ImageDataIO", "WormCurveFinder",
+    pkg_list=  []
+    
+    for pkg = ["MHDIO", "ImageDataIO", "WormCurveFinder",
         "WormFeatureDetector", "SegmentationTools", "CaAnalysis", "ND2Process"]
+        push!(pkg_list, Pkg.PackageSpec(url="git@github.com:flavell-lab/$(pkg).jl.git"))
+    end
+        
     # adding
     for pkg = pkg_list
-        Pkg.add(Pkg.PackageSpec(url="git@github.com:flavell-lab/$(pkg).jl.git"))
+        Pkg.add(pkg)
     end
-
     # precompiling
     for pkg = pkg_list
         eval(:(using $(Symbol(pkg))))
