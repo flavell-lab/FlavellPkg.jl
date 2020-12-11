afca63481e63d9de6ae3a3ce5c52a975d3f3ab71
Adam Atanas <goldenratio1618@users.noreply.github.com>
2020-12-11 15:54:38 -0600

    Added `SLURMManager` dependency

--- src/imaging.jl
+++ src/imaging.jl
@@ -5,7 +5,7 @@ function install_imaging()
     # add private pkg
     for pkg = ["MHDIO", "ImageDataIO", "WormCurveFinder",
         "WormFeatureDetector", "SegmentationTools", "CaAnalysis",
-        "ND2Process", "RegistrationGraph", "ExtractRegisteredData"]
+        "ND2Process", "SLURMManager", "RegistrationGraph", "ExtractRegisteredData"]
         push!(pkg_list, Pkg.PackageSpec(name=pkg,
             url="git@github.com:flavell-lab/$(pkg).jl.git"))
     end
