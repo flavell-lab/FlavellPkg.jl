4fa6fa10a6a379244e7a6a437044f98c9b1873a0
Adam Atanas <goldenratio1618@users.noreply.github.com>
2021-03-27 17:12:00 -0400

    Add BehaviorDataNIR and TotalVariation

--- src/imaging.jl
+++ src/imaging.jl
@@ -5,13 +5,13 @@ function install_imaging()
     # add private pkg
     for pkg = ["GPUFilter", "MHDIO", "FFTRegGPU", "ImageDataIO", "WormCurveFinder", "Clustering",
         "WormFeatureDetector", "SegmentationTools", "ND2Process", "SLURMManager",
-        "RegistrationGraph", "ExtractRegisteredData", "CaAnalysis"]
+        "RegistrationGraph", "ExtractRegisteredData", "CaAnalysis", "BehaviorDataNIR"]
         push!(pkg_list, Pkg.PackageSpec(name=pkg,
             url="git@github.com:flavell-lab/$(pkg).jl.git"))
     end
 
     for pkg = ["Statistics", "StatsBase", "DelimitedFiles", "Cairo", "Interact", "WebIO",
-        "Plots", "Dates", "JLD2"]
+        "Plots", "Dates", "JLD2", "TotalVariation"]
        push!(pkg_list, pkg)
    end
 
