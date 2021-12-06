f1fb42ebf9f480a74d388660417bc8af1b34c526
goldenratio1618 <adamatanas@gmail.com>
2021-12-06 11:46:31 -0500

    Install base packages before installing lab packages.

--- src/default.jl
+++ src/default.jl
@@ -3,17 +3,22 @@ import Pkg
 function install_default(use_julia_conda=true)
     pkg_list = []
 
-    # add private pkg
-    for pkg = ["FlavellBase"]
-        push!(pkg_list, Pkg.PackageSpec(name=pkg,
-            url="git@github.com:flavell-lab/$(pkg).jl.git"))
-    end
-
     # add registered pkg
     for pkg = ["IJulia", "HDF5", "NaNMath", "LsqFit", "Optim", "Interpolations",
          "Distributions", "DataStructures", "BenchmarkTools", "ProgressMeter",
          "Images", "Clustering", "MultivariateStats", "CUDA"]
         push!(pkg_list, pkg)
+    end
+
+    # adding
+    for pkg = pkg_list
+        Pkg.add(pkg)
+    end
+
+    # add private pkg
+    for pkg = ["FlavellBase"]
+        push!(pkg_list, Pkg.PackageSpec(name=pkg,
+            url="git@github.com:flavell-lab/$(pkg).jl.git"))
     end
 
     # adding
