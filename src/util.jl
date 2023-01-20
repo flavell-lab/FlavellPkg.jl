8aff13ddb9237d6ac082050227ade37fe8527e6d
Jungsoo Kim <urlicht@users.noreply.github.com>
2023-01-20 15:26:52 -0500

    Update util.jl
    remove git return string; add result print

--- src/util.jl
+++ src/util.jl
@@ -1,12 +1,17 @@
-function check_develop(remote_url)
+function check_develop(remote_url, verbose=true)
     # Check if the `develop` branch exists in the remote repository
     branch_exists = false
 
     try
-        cmd_str = String(read(Cmd(`git ls-remote --heads $remote_url develop`)))
-        println(cmd_str)
+        cmd_str = String(read(Cmd(`git ls-remote --heads $remote_url develop`))
         branch_exists = occursin("develop", cmd_str)
     catch e
+    end
+    
+    if branch_exists
+        println("$remote_url - has develop")
+    else
+        println("$remote url - does not have develop")
     end
 
     branch_exists
