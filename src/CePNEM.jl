81fe2b99f74b1e4b93f12944aed6fd834212f225
Adam Atanas <adamatanas@gmail.com>
2023-01-20 15:47:20 -0500

    Added CePNEM pacakges

--- src/CePNEM.jl
+++ src/CePNEM.jl
@@ -1,16 +1,13 @@
 function install_imaging(install_dev_branch=false)
     pkg_list = Pkg.Types.PackageSpec[]
 
-    for pkg = ["Statistics", "StatsBase", "DelimitedFiles", "Cairo", "Interact", "WebIO",
-        "Plots", "Dates", "JLD2", "VideoIO", "FFTW", "GLMNet", "InformationMeasures", "LsqFit",
-        "Rotations", "CoordinateTransformations", "ImageTransformations"]
+    for pkg = ["ColorVectorSpace", "MultipleTesting", "Gen", "UMAP", "ImageShow", "ImageCore", "Combinatorics",
+            "HypothesisTests", "SpecialFunctions", "FileIO", "StatsPlots", "GLM", "DataFrames", "LinearAlgebra"]
         push!(pkg_list, Pkg.PackageSpec(name=pkg))
     end
 
     # add private pkg
-    for pkg = ["MHDIO","TotalVariation", "SegmentationStats", "GPUFilter", "NRRDIO", "FFTRegGPU", "ImageDataIO", "WormCurveFinder", "Clustering", "UNet2D",
-        "WormFeatureDetector", "SegmentationTools", "ND2Process", "SLURMManager",
-        "RegistrationGraph", "ExtractRegisteredData", "CaAnalysis", "BehaviorDataNIR"]
+    for pkg = ["FlavellConstants","ANTSUNData","NeuroPALData","AnalysisBase","EncoderModel","EncoderModelGen","CePNEMAnalysis"]
 
         remote_url = "git@github.com:flavell-lab/$(pkg).jl.git"
         if install_dev_branch && check_develop(remote_url)
